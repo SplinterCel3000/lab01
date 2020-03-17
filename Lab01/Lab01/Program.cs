f4777de20ad2a309c41d5a8d6538ac1a3a7b6288
Brandon J <splintercel3000@gmail.com>
2020-03-17 11:46:24 -0700

    Got input wokring for the array and it populates.

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -13,17 +13,31 @@ namespace Lab01
         static void StartSequence()
         {
             string answer = "";
+            Console.WriteLine("Enter a number greater than zero.");
+            answer = Console.ReadLine();
             try
             {
-                Console.WriteLine("Enter a number greater than zero.");
-                answer = Console.ReadLine();
+                
                 int number = Convert.ToInt32(answer);
+                int[] array = new int[number];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    string input = Console.ReadLine();
+                    int convert = Convert.ToInt32(input);
+                    array[i] = convert;
+                }
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
             }
             catch (Exception)
             {
                 Console.WriteLine("Thats not a number...");
               
             }
+
+
         }
     }
 }
